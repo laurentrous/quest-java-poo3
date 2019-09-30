827ab6768ccc11d9d245fcb048bf572888a194d0
Laurent Rous <rouslaurent@gmail.com>
2019-09-30 15:48:51 +0200

    modif nature et eagle et ajout fly

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -21,4 +21,60 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+
+    /**
+     * takeof -> isFlying = false and altitude == 0
+     */
+    @Override
+    public void takeOff() {
+        if(!this.flying && this.altitude == 0) {
+            this.flying = true;
+            System.out.printf("%s takes off in the sky%n", this.getName());
+        }
+    }
+
+    /**
+     * flie upward
+     * @param meters altitude increase
+     * @return altitude in meters
+     */
+    @Override
+    public int ascend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.min(this.altitude + meters, 325);
+            System.out.printf("%s flies upward, alititude : %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+
+    /**
+     * flie downward
+     * @param meters altitude decrease
+     * @return altitude in meters
+     */
+    @Override
+    public int descend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.min(this.altitude - meters, 325);
+            System.out.printf("%s flies downward, alititude : %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+
+    /**
+     * land -> isFlying = true and altitude > 0
+     */
+    @Override
+    public void land() {
+        if(this.flying && this.altitude > 0) {
+            if(this.altitude == 1) {
+                System.out.printf("%s lands on the ground%n", this.getName());
+                this.flying = false;
+            } else {
+                System.out.printf("%s is too hight, it can't land%n", this.getName());
+            }
+            
+        }
+    }
+
 }
